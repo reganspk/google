76edbb3ff4906c4a36e8b6787bf2ded169d658e3
reganspk <sachinstha2015@gmail.com>
2020-08-22 10:32:30 +0545

    Google

---
+++ build/precache-manifest.cd906086e62e2c0b03817e42c3c7aee3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "013689dfd4ce4f2e86afc272ffba17a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25bfdb11857496572350",
+    "url": "/static/css/main.367906a7.chunk.css"
+  },
+  {
+    "revision": "d6c27aba9574342e4127",
+    "url": "/static/js/2.e82d37fd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.e82d37fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25bfdb11857496572350",
+    "url": "/static/js/main.55a82399.chunk.js"
+  },
+  {
+    "revision": "42236b3c5a0e9054fb96",
+    "url": "/static/js/runtime-main.0c82e416.js"
+  }
+]);
---
+++ src/keys.js
@@ -0,0 +1,2 @@
+export const API_KEY = "AIzaSyCt_u87tTd9NurpZsZwuH1wMpsNvgLA5KE";
+export default API_KEY;
